b691f7c3315f7e67f1043b2103b32739a7469934
trh-thanh30 <tranhuuthanhcp@gmail.com>
2026-07-20 21:51:28 +0700

    feat(i18n): add localeDetection option to routing configuration

--- apps/admin/src/i18n/routing.ts
+++ apps/admin/src/i18n/routing.ts
@@ -3,6 +3,7 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["vi", "en"],
   defaultLocale: "vi",
+  localeDetection: false,
 });
 
 export type AppLocale = (typeof routing.locales)[number];
